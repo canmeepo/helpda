08cf57b2d4f83299167505eca57d04e1ed29b8ee
canmeepo <canmeepo@gmail.com>
2019-02-25 01:35:17 +0300

    1.0.6

--- src/core/reduce.ts
+++ src/core/reduce.ts
@@ -1,10 +1,10 @@
 const reduce = <T, U>(fn: (init: U, val: T, i: number, a: T[]) => U, iv: U, arr: T[]): U => {
-    for (let i = 0; i < arr.length; i++) {
-      const item = arr[i];
-      iv= fn(iv, item, i, arr);
-    }
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    iv = fn(iv, item, i, arr);
+  }
 
-    return iv;
-}
+  return iv;
+};
 
 export default reduce;
